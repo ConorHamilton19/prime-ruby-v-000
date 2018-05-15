5e7b6d351ea961ac3b97a5e9254b1de6b0e32e28
Conor Hamilton <conhamil19@gmail.com>
2018-05-15 07:29:23 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
-def prime?(n)
- (2..n/2).none? do |i| 
-   n % i == 0
- end
- end
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
+end
  
